ba48fc8959b411a5fb6c06a0552ef2602a297916
nglow <nglow90@gmail.com>
2021-06-20 12:52:05 +0900

    Done

---
+++ README.md
@@ -0,0 +1,7 @@
+# Web App
+## Procedure
+1. 프로젝트 환경설정
+2. 도메인 분석 설계
+
+## Reference
+[실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발](https://www.inflearn.com/course/스프링부트-JPA-활용-1/dashboard)
---
+++ src/test/java/study/webapp/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package study.webapp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+//@Rollback(false) // 테스트코드가 돌아간 후 Rollback하지 않고 DB에 적용됨
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원 테스트")
+    public void testMember() {
+        var member = new Member();
+        member.setUsername("memberA");
+
+        var id = memberRepository.save(member);
+        var memberRetrieved = memberRepository.find(id);
+
+        assertThat(memberRetrieved.getId()).isEqualTo(member.getId());
+        assertThat(memberRetrieved.getUsername()).isEqualTo(member.getUsername());
+        assertThat(memberRetrieved).isEqualTo(member);
+
+    }
+}
